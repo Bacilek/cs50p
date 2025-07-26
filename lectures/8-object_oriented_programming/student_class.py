921f89f47ca42b37ce269ba7187e646950ea2922
Marek Doupovec <DoupovecMarek@seznam.cz>
2025-07-26 22:40:56 +0200

    /8/student_class.py

--- lectures/8-object_oriented_programming/student_class.py
+++ lectures/8-object_oriented_programming/student_class.py
@@ -1,21 +1,38 @@
 class Student:
     # "def underscore underscore" -> "dunder" function
     # __init__ = function initializing an instance of a class (object), "self" argument represents that new object
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
+        # if name == ""
+        if not name:
+            raise ValueError("Error: Missing name.")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Error: Invalid House.")
         self.name = name
         self.house = house
+        self.patronus = patronus
+    # when a function wants to see my object as a string (print, ...)
+    def __str__(self):
+        return f"{self.name} from {self.house} with {self.patronus}"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    # name == "" and/or house not in valid houses
+    if student is not None:
+        print(student) # __str__
+    else:
+        print("Student creation failed")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house)  # constructor call -> instantiate Student object
-    return student
+    patronus = input("Patronus: ")
+    try:
+        return Student(name, house, patronus)  # constructor call -> instantiate Student object
+    except ValueError as e:
+        print(e)  # prints the raises
+        return None
 
 
 if __name__ == "__main__":
