028455f5f52cb5fae7bd3a1e93a075865a91da61
Marek Doupovec <DoupovecMarek@seznam.cz>
2025-07-29 23:50:32 +0200

    instance variables and methods

--- lectures/8-object_oriented_programming/packages.py
+++ lectures/8-object_oriented_programming/packages.py
@@ -1,9 +1,9 @@
 class Package:
     def __init__(self, id, sender, recipient, weight):
-        if not sender or not recipient:
-            raise ValueError("Error: Missing sender or receiver.")
         if id < 0:
             raise ValueError("Error: Invalid ID.")
+        if not sender or not recipient:
+            raise ValueError("Error: Missing sender or receiver.")
         if weight <= 0:
             raise ValueError("Error: Invalid weight.")
         self.id = id
@@ -12,13 +12,17 @@ class Package:
         self.weight = weight
     
     def __str__(self):
-        return f"Package {self.id} is from {self.sender} to {self.recipient} with weight of {self.weight} kilograms."        
+        return f"Package {self.id} is from {self.sender} to {self.recipient} with weight of {self.weight} kilograms and costs ${self.calculate_cost(5)}."        
 
-
+    def calculate_cost(self, cost_per_kg):
+        return self.weight * cost_per_kg
+        
+        
 def main():
     packages = [
         Package(1, "Alice", "Bob", 10),
         Package(2, "Charlie", "Dave", 5),
+        Package(3, "Eve", "Frank", 2.5)
     ]
     
     for package in packages:
