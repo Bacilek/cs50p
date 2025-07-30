b4d44568656346eb4feb17327462233b16f0df26
Marek Doupovec <DoupovecMarek@seznam.cz>
2025-07-30 13:28:20 +0200

    added asserts to test_seasons.py

--- problems/8/seasons/test_seasons.py
+++ problems/8/seasons/test_seasons.py
@@ -7,9 +7,9 @@ def main():
 
 
 def test_calculate():
-    date_to_text(date(2025, 7, 30), date(2024, 7, 30)) == "five hundred twenty-five thousand, six hundred"
-    date_to_text(date(2025, 7, 30), date(2023, 7, 30)) == "one million, fifty-two thousand, six hundred forty"
-    date_to_text(date(2025, 7, 30), date(2000, 12, 31)) == "twelve million, nine hundred twenty-six thousand, eight hundred eighty"
+    assert date_to_text(date(2025, 7, 30), date(2024, 7, 30)) == "five hundred twenty-five thousand, six hundred"
+    assert date_to_text(date(2025, 7, 30), date(2023, 7, 30)) == "one million, fifty-two thousand, six hundred forty"
+    assert date_to_text(date(2025, 7, 30), date(2000, 12, 31)) == "twelve million, nine hundred twenty-six thousand, eight hundred eighty"
 
 
 if __name__ == "__main__":
